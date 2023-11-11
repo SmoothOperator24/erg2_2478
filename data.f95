a4f9c2d246522ef6e6d54fd352ac2c9efd6b5d75
Pavlos Kalonis <pkalonis@uth.gr>
2023-11-11 19:42:19 +0200

    c4 - Read and display all items (up to 50)

--- data.f95
+++ data.f95
@@ -1,19 +1,25 @@
 program rainfall
 implicit none
 
-integer:: i
-character(15):: town(5)
-integer:: rain(5)
+integer:: i, Nmax, st
 
+character(15):: town(50)
+integer:: rain(50)
+
+Nmax=0
+i=0
 open(10,File='C:\Users\pkalo\Documents\rainfall\data.txt')
-read(10,*)
-do i=1,5
-read(10,*) town(i), rain(i)
-end do
+ read(10,*)
+  do
+   i=i+1
+   read(10,*,iostat=st) town(i), rain(i)
+   if (st==-1) exit
+   Nmax=Nmax+1  
+  end do
 close(10)
 
-do i=1,5
-write(*,*) town(i), rain(i)
+do i=1,Nmax
+    write(*,*) town(i), rain(i)
 end do
 
 end program  
